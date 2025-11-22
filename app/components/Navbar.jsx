c235603402712b6ca8da0d8dc0f6ad81b2a84e88
lurvdroid <49570063+lurvdroid@users.noreply.github.com>
2025-11-22 21:29:05 +0300

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,5 +1,12 @@
-\"use client\";
+"use client";
+
 import ThemeToggle from "./ThemeToggle";
-export default function Navbar(){
-return(<nav className="w-full p-4 shadow bg-white dark:bg-gray-800 flex justify-between items-center"><p className="font-bold">ThreadLite</p><ThemeToggle/></nav>);
+
+export default function Navbar() {
+  return (
+    <nav className="w-full p-4 shadow bg-white dark:bg-gray-800 flex justify-between items-center">
+      <p className="font-bold">DroidLite</p>
+      <ThemeToggle />
+    </nav>
+  );
 }
