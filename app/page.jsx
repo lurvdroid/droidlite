087e1ef79b6ea07cb6dc725274a4f76ab605ea61
lurvdroid <49570063+lurvdroid@users.noreply.github.com>
2025-11-22 21:28:24 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,4 @@
-\"use client\";
-import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import CreatePost from "./components/CreatePost";
-import Post from "./compon"use client";
+"use client";
 
 import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
@@ -42,14 +38,4 @@ export default function Home() {
       </main>
     </div>
   );
-}
-ents/Post";
-import { savePosts, loadPosts } from "./lib/storage";
-
-export default function Home(){
-  const [posts,setPosts]=useState([]);
-  useEffect(()=>{setPosts(loadPosts());},[]);
-  const addPost=(text)=>{const p=[{text,id:Date.now()},...posts];setPosts(p);savePosts(p);};
-  const deletePost=(id)=>{const p=posts.filter(x=>x.id!==id);setPosts(p);savePosts(p);};
-  return(<div><Navbar/><main className="max-w-lg mx-auto p-4"><CreatePost addPost={addPost}/>{posts.map(post=>(<Post key={post.id} data={post} onDelete={()=>deletePost(post.id)}/>))}</main></div>);
 }
